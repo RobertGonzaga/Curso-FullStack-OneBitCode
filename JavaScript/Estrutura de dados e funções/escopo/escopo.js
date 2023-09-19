cfad0298042f01d6c469cb548658b2fc1a6f478d
Robert Gonzaga <robertgonzagaotop@gmail.com>
2023-09-19 19:32:27 -0400

    escopo

---
+++ JavaScript/Estrutura de dados e funções/escopo/escopo.js
@@ -0,0 +1,16 @@
+let pokemon = "Charmander";
+
+function evoluir() {
+  pokemon = "Charmeleon";
+}
+
+console.log(pokemon);
+evoluir();
+console.log(pokemon);
+
+function criarAnimal() {
+  let animal = "gato";
+}
+
+criarAnimal();
+console.log(animal);
