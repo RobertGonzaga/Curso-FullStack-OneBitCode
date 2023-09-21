275bad02c6bc49c5257c4fb9ac2fa1df76eaf0bb
Robert Gonzaga <robertgonzagaotop@gmail.com>
2023-09-21 11:56:52 -0400

    otimizacao exercicio 9

--- JavaScript/Estrutura de dados e funções/exercicio 9/calculadora.js
+++ JavaScript/Estrutura de dados e funções/exercicio 9/calculadora.js
@@ -1,29 +1,35 @@
-let opcao = "";
-let resultado = "";
-
-function areaTriangulo(base, altura) {
+function areaTriangulo() {
+  const base = parseFloat(prompt("Valor da base:"));
+  const altura = parseFloat(prompt("Valor da altura:"));
   return (base * altura) / 2;
 }
 
-function areaRetangulo(base, altura) {
+function areaRetangulo() {
+  const base = parseFloat(prompt("Valor da base:"));
+  const altura = parseFloat(prompt("Valor da altura:"));
   return base * altura;
 }
 
-function areaQuadrado(lado) {
+function areaQuadrado() {
+  const lado = parseFloat(prompt("Valor do lado:"));
   return lado * lado;
 }
 
-function areaTrapezio(baseMaior, baseMenor, altura) {
+function areaTrapezio() {
+  const baseMaior = parseFloat(prompt("Valor da base maior:"));
+  const baseMenor = parseFloat(prompt("Valor da base menor:"));
+  const altura = parseFloat(prompt("Valor da altura:"));
   return ((parseFloat(baseMaior) + parseFloat(baseMenor)) * altura) / 2;
 }
 
-function areaCirculo(raio) {
+function areaCirculo() {
+  const raio = parseFloat(prompt("Valor do raio:"));
   const pi = 3.14;
   return pi * (raio * raio);
 }
 
-do {
-  opcao = prompt(
+function exibirMenu() {
+  return prompt(
     "**Calculadora Geométrica**" +
       "\n\nEscolha uma das formas geométricas abaixo para calcular sua área:" +
       "\n1. Triângulo" +
@@ -33,41 +39,40 @@ do {
       "\n5. Círculo" +
       "\n6. Sair"
   );
-  switch (opcao) {
-    case "1":
-      resultado = areaTriangulo(
-        prompt("Valor da base:"),
-        prompt("Valor da altura:")
-      );
-      alert("Área do triângulo = " + resultado);
-      break;
-    case "2":
-      resultado = areaRetangulo(
-        prompt("Valor da base:"),
-        prompt("Valor da altura:")
-      );
-      alert("Área do retângulo = " + resultado);
-      break;
-    case "3":
-      resultado = areaQuadrado(prompt("Valor do lado:"));
-      alert("Área do quadrado = " + resultado);
-      break;
-    case "4":
-      resultado = areaTrapezio(
-        prompt("Valor da base maior:"),
-        prompt("Valor da base menor: "),
-        prompt("Valor da altura:")
-      );
-      alert("Área do trapézio = " + resultado);
-      break;
-    case "5":
-      resultado = areaCirculo(prompt("Valor do raio:"));
-      alert("Área do círculo = " + resultado);
-      break;
-    case "6":
-      alert("Encerrando...");
-      break;
-    default:
-      alert("Insira uma opcao válida!");
-  }
-} while (opcao !== "6");
+}
+
+function executar() {
+  let opcao = "";
+  do {
+    opcao = exibirMenu();
+    let resultado;
+
+    switch (opcao) {
+      case "1":
+        resultado = areaTriangulo();
+        break;
+      case "2":
+        resultado = areaRetangulo();
+        break;
+      case "3":
+        resultado = areaQuadrado();
+        break;
+      case "4":
+        resultado = areaTrapezio();
+        break;
+      case "5":
+        resultado = areaCirculo();
+        break;
+      case "6":
+        alert("Encerrando...");
+        break;
+      default:
+        alert("Insira uma opcao válida!");
+    }
+    if (resultado) {
+      alert("Resultado: " + resultado);
+    }
+  } while (opcao !== "6");
+}
+
+executar();
